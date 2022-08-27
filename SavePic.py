05f2e614ac61e210c5387ba79199bf390cc48bd4
drenix-drenix <109510841+drenix-drenix@users.noreply.github.com>
2022-08-27 20:42:07 +0300

    Update SavePic.py

--- SavePic.py
+++ SavePic.py
@@ -5,7 +5,7 @@ class SaverMod(loader.Module):
     strings = {"name": "SavePic"} 
     
     async def svcmd(self, m): 
-        ".sv + реплай на самоуничтожающееся фото, чтобы сохранить" 
+        "реплай на самоуничтожающееся фото, чтобы сохранить" 
         reply = await m.get_reply_message() 
         if not reply or not reply.media.ttl_seconds: return await m.edit("Реплаем на самоуничтожающееся фото! ")
         await m.delete() 
